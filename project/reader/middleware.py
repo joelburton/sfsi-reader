cfb787e20b86343ee0d83fc456f786f24f8e9ee2
Joel Burton <joel@joelburton.com>
2015-02-19 08:22:29 -0500

    Don't use regexes for login middleware.

--- project/reader/middleware.py
+++ project/reader/middleware.py
@@ -4,15 +4,13 @@ Additional middleware for our site.
 
 from django.http import HttpResponseRedirect
 from django.conf import settings
-from re import compile
 
 
-EXEMPT_URLS = [compile(settings.LOGIN_URL.lstrip('/'))]
-if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
-    EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
+EXEMPT_URLS = [settings.LOGIN_URL] + getattr(settings, 'LOGIN_EXEMPT_URLS', [])
+EXEMPT_URLS = [url.lstrip('/') for url in EXEMPT_URLS]
 
 
-class LoginRequiredMiddleware:
+class LoginRequiredMiddleware(object):
     """
     Middleware that requires a user to be authenticated to view any page other
     than LOGIN_URL. Exemptions to this requirement can optionally be specified
@@ -23,8 +21,9 @@ class LoginRequiredMiddleware:
     loaded. You'll get an error if they aren't.
     """
 
-    def process_request(self, request):
-        if not request.user.is_authenticated():
+    @staticmethod
+    def process_request(request):
+        if request.user.is_anonymous():
             path = request.path_info.lstrip('/')
-            if not any(m.match(path) for m in EXEMPT_URLS):
+            if not any(path.startswith(exempt) for exempt in EXEMPT_URLS):
                 return HttpResponseRedirect(settings.LOGIN_URL)
