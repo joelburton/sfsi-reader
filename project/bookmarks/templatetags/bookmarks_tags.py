ea0c8a5264d06c26081c3cba37fc5f084822d35b
Joel Burton <joel@joelburton.com>
2015-02-21 12:38:21 -0500

    Change to simple inclusion tag.
    ZOMG so much less code!

--- project/bookmarks/templatetags/bookmarks_tags.py
+++ project/bookmarks/templatetags/bookmarks_tags.py
@@ -1,74 +1,31 @@
 from django import template
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import resolve, reverse
-from django.template import loader
+from django.core.urlresolvers import reverse
 
 from ..models import Bookmark
 
+
 register = template.Library()
 
-@register.tag
-def bookmark_widget(parser, token):
-    """Inserts bookmark widget.
 
-    If ajax=True, inserts a static widget that gets the initial bookmarked/not-bookmarked
-    state via an AJAX call. This allows the page to be cached for multiple users, as
-    the bookmark status is not in the HTML.
+@register.inclusion_tag('bookmarks/includes/bookmark.html', takes_context=True)
+def bookmark_widget(context, obj, ajax=False):
 
-    If ajax=False, the bookmark status is calculated here; this saves an AJAX call
-    but makes the page not cacheable across users.
-    """
+    content_type_id = ContentType.objects.get_for_model(obj).pk
+    obj_id = obj.pk
+    user = context['request'].user
 
-    tokens = token.split_contents()
-    print(tokens)
-    format_err = "{0} tag requires syntax: {{% {0} for <object> [ajax=<True|False>] %}}".format(tokens[0])
+    # For ajax widget, we have it get the bookmark status via AJAX call
+    bookmarked = None if ajax else Bookmark.is_bookmarked(user, content_type_id, obj_id)
 
-    try:
-        if len(tokens) == 4:
-            ajax_opt = tokens.pop()
-            if ajax_opt == 'ajax':
-                ajax = 'True'
-            else:
-                ajax_str, ajax = ajax_opt.split("=")
-                if ajax_str != 'ajax':
-                    raise ValueError
-        else:
-            ajax = "False"  # will get resolved as variable -> bool(False)
+    kwargs = {'content_type_id': content_type_id, 'object_id': obj_id}
+    toggle_url = reverse('bookmarks:bookmark_toggle', kwargs=kwargs)
+    get_url = reverse('bookmarks:bookmark_get', kwargs=kwargs)
 
-        tag_name, for_str, obj = tokens
-        if for_str != 'for':
-            raise ValueError
-
-    except ValueError:
-            raise template.TemplateSyntaxError(format_err)
-
-    return ObjectBookmarkWidget(obj, ajax)
-
-
-class ObjectBookmarkWidget(template.Node):
-    def __init__(self, obj, ajax):
-        self.obj = obj
-        self.ajax = ajax
-
-    def render(self, context):
-        obj = template.resolve_variable(self.obj, context)
-        ajax = template.resolve_variable(self.ajax, context)
-        ct = ContentType.objects.get_for_model(obj)
-        user = context['request'].user
-
-        context.push()
-        context['object'] = obj
-        context['content_type_id'] = ct.pk
-
-        kwargs = {'content_type_id': ct.pk, 'object_id': obj.pk}
-        context['toggle_url'] = reverse('bookmarks:bookmark_toggle', kwargs=kwargs)
-        context['get_url'] = reverse('bookmarks:bookmark_get', kwargs=kwargs)
-
-        context['ajax'] = ajax
-        if not ajax:
-            context['bookmarked'] = Bookmark.is_bookmarked(user, ct.pk, obj.pk)
-
-        output = loader.render_to_string("bookmarks/includes/bookmark.html", context)
-        context.pop()
-
-        return output
+    return {
+        'content_type_id': content_type_id,
+        'toggle_url': toggle_url,
+        'get_url': get_url,
+        'ajax': ajax,
+        'bookmarked': bookmarked
+    }
